f23bd2ce7ef46298a99cff76642ba5f04b2bb235
ItIsMLiu <mliu.design@outlook.com>
2023-12-11 10:21:04 +0000

    createTimeBlocks function updated so that timeblocks change colour as time change.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # daily-planner-app
 A daily planner app that allows users to save events for every working hour of the day.
+
+getHours Method: https://www.w3schools.com/jsref/jsref_gethours.asp
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,15 @@
 var displayTimeEl = $('#display-time');
 var allTimeBlocks = $('.container');
-var timeBlockEl = [9, 10, 11, 12, 1, 2, 3,4, 5]
+var timeBlockEl = [9, 10, 11, 12, 1, 2, 3, 4, 5]
+
 // displaying day and time
 function displayTime() {
     var timeNow = dayjs().format('dddd, MMMM DD, YYYY [at] h:mm A');
     displayTimeEl.text(timeNow);
 }
-
 setInterval(displayTime, 1000);
 
+// function for creating timeblocks for each working hour in html
 function createTimeBlocks(X) {
     var newDiv = $('<div>');
     var newLabel = $('<lable>')
@@ -40,8 +41,31 @@ function createTimeBlocks(X) {
     newSaveBtn.attr('type', 'save');
     newSaveBtn.addClass('col saveBtn');
     newSaveBtn.append($('<i class="fas fa-save"></i>'));
+
+    //getting the current hour
+    var dt = new Date();
+    var currentHour = dt.getHours();
+
+    if (X > 8 && currentHour < 13 && X === currentHour){
+        newTextArea.addClass('present');
+    } else if (X > 8 && currentHour < 13 && X < currentHour){
+        newTextArea.addClass('past');
+    } else if (X > 8 && currentHour < 13 && X > currentHour){
+        newTextArea.addClass('future');
+    } else if (X > 8 && currentHour > 12){
+        newTextArea.addClass('past');
+    } else if (X < 6 && currentHour > 12 && X + 12 === currentHour){
+        newTextArea.addClass('present');
+    } else if (X < 6 && currentHour > 12 && X + 12 < currentHour){
+        newTextArea.addClass('past');
+    } else if (X < 6 && currentHour > 12 && X + 12 > currentHour){
+        newTextArea.addClass('future');
+    } else if (X < 6 && currentHour < 13){
+        newTextArea.addClass('future');
+    }
 }
 
+//loop for creating timeblocks automatically in html
 for (var i = 0; i < timeBlockEl.length; i++) {
     createTimeBlocks(timeBlockEl[i])
 }
