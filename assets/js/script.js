e393854c7fce470942c7a73bba913e1f48e600fa
ItIsMLiu <mliu.design@outlook.com>
2023-12-12 00:28:09 +0000

    js codes refined.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
+//declare global variable
 var displayTimeEl = $('#display-time');
 var allTimeBlocks = $('.container');
-var timeBlockEl = [9, 10, 11, 12, 1, 2, 3, 4, 5]
+var timeBlockEl = [9, 10, 11, 12, 1, 2, 3, 4, 5];
 
 // displaying day and time
 function displayTime() {
@@ -36,9 +37,11 @@ function createTimeBlocks(X) {
 
     newDiv.append(newTextArea);
     newTextArea.addClass('col-8');
+    newTextArea.attr('name', X);
 
     newDiv.append(newSaveBtn);
     newSaveBtn.attr('type', 'save');
+    newSaveBtn.attr('id', X);
     newSaveBtn.addClass('col saveBtn');
     newSaveBtn.append($('<i class="fas fa-save"></i>'));
 
@@ -73,3 +76,108 @@ function createTimeBlocks(X) {
 for (var i = 0; i < timeBlockEl.length; i++) {
     createTimeBlocks(timeBlockEl[i]);
 }
+
+var msgDiv = $('#msg');
+var saveBtnEl = $('.saveBtn');
+var allTasks = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
+
+function renderTasks() {
+    for (var i = 0; i < timeBlockEl.length; i++){
+        var hourlyTask = allTasks[i];
+        $('.container').children().eq(i+1).children('textarea').text(hourlyTask);
+    }
+}
+
+function displayMessage (message) {
+    msgDiv.text(message);
+}
+
+function init() {
+    var storedTasks = JSON.parse(localStorage.getItem("allTasks"));
+    if (storedTasks !== null) {
+        allTasks = storedTasks;
+    }
+    renderTasks()
+}
+
+function storedTasks () {
+    localStorage.setItem("allTasks", JSON.stringify(allTasks));
+}
+
+saveBtnEl.on('click', function (event) {
+    event.preventDefault();
+    var element = event.target;
+    if (element.matches("button") === true) {
+        var btnID = element.getAttribute("id");
+        if (btnID > 8) {
+            var textAreaID = String(btnID + "amInput");
+        } else {
+            var textAreaID = String(btnID + "pmInput");
+        }
+        
+        var selectedTextAreaEl = document.getElementById(textAreaID);
+        var taskText = selectedTextAreaEl.value.trim();
+        
+        if (btnID > 8){
+            allTasks.splice(btnID-9, 1);
+            allTasks.splice(btnID-9, 0, taskText);
+        } else if (btnID < 6){
+            allTasks.splice(btnID+3, 1);
+            allTasks.splice(btnID+3, 0, taskText);
+        }
+
+        displayMessage("Task Successfully Saved!")
+        storedTasks();
+        renderTasks();
+    }
+})
+
+/*
+
+
+init();
+
+function displayMessage (type, message) {
+    msgDiv.text(message);
+    msgDiv.attr('class', type);
+}
+
+function init() {
+    var storedTasks = JSON.parse(localStorage.getItem("dailyTasks"));
+    if (storedTasks !== null) {
+        dailyTasks = storedTasks;
+    }
+}
+
+
+
+
+
+saveBtnEl.on('click', function (event) {
+    event.preventDefault();
+    var element = event.target;
+    var saveID = element.getAttribute("id");
+    if (element.matches("button") === true) {
+        if (saveID < 13) {
+            var textAreaID = saveID + "amInput";
+            console.log (textAreaID);
+            localStorage.setItem("task", JSON.stringify($textAreaIA));
+        } else {
+            var textAreaID = saveID + "pmInput";
+            console.log (textAreaID);
+            localStorage.setItem("task", JSON.stringify($textAreaIA));
+        }
+    }
+}
+    
+
+});
+
+    if (X > 8) {
+        var amTextAreaID = X + "amInput";
+    } else {
+        var pmTextAreaID = X + "pmInput";
+    }
+   
+    console.log (amTextAreaID);
+*/
